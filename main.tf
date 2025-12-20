2523d3d27aec00c46877f3f5fe96a29b094a7e4f
Jyotsna Deshmukh <deshmukhakshay23@gamil.com>
2025-12-21 02:08:03 +0530

    instance name change

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "my-instance" {
   ami           = "ami-00ca570c1b6d79f36"
   instance_type = "t3.micro"
   tags = {
-    "Name" = "MyEC2Instance"
+    "Name" = "MyEC2Instance2"
   }
 }
