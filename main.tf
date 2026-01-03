54ab24d8c0455ac2c7682165e8d33c6187836207
Akshay Deshmukh <deshmukhakshay23@gamil.com>
2026-01-04 00:32:33 +0530

    adding variables

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+
 }
 
 resource "aws_instance" "my-instance" {
+  region = var.region
   count = 2
   ami           = "ami-00ca570c1b6d79f36"
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   tags = {
     "Name" = "MyEC2Instance ${count.index}"
     "Type" = "Learning"
