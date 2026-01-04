8cf2afbe70be6e34158b367191ee5459a9be5b03
Akshay Deshmukh <deshmukhakshay23@gamil.com>
2026-01-05 00:48:05 +0530

    adding more vairables

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "my-instance" {
   region = var.region
-  count = 2
-  ami           = "ami-00ca570c1b6d79f36"
+  count = var.numberofinstance
+  ami           = var.ami
   instance_type = var.instance_type
   tags = {
     "Name" = "MyEC2Instance ${count.index}"
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "instance_type" {
     type = string
     default = "t3.micro"
 }
+
+variable "ami" {
+    type = string
+    default = "ami-00ca570c1b6d79f36"
+}
+
+variable "numberofinstance" {
+    type = number
+    default = 2
+}
